6727736696bbfdd930aeaf4a2f8c2acd6dde352a
hyperkinoko <github.hyperkinoko@kinokodata.net>
2025-01-30 05:01:14 +0900

    売上モデルの実装

---
+++ src/lib/types/sales.ts
@@ -0,0 +1,10 @@
+import type {Product} from "$lib/types/product";
+import type {ProductCategory} from "$lib/types/productCategory";
+
+export interface DailySales {
+    id: number;                 // ID
+    saleDate: string;           // 売上日（日付文字列）
+    product: Product;           // 商品
+    quantity: number;           // 売上個数
+    totalAmount: number;       // 売上額
+}
