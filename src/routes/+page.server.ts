3f58718c7879d7f18d38e1fee55d5343d081455e
hyperkinoko <github.hyperkinoko@kinokodata.net>
2025-04-19 17:07:35 +0900

    コメントを修正

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,5 @@
 import { redirect } from "@sveltejs/kit";
 
 export function load() {
-    throw redirect(302, "/sales/analysis");  // /products ページにリダイレクト
+    throw redirect(302, "/sales/analysis");  // sales/analysis ページにリダイレクト
 }
